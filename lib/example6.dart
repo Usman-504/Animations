fd730719ba3f287dfec1637457627122d3d5c9f4
Muhammad Usman <ahmadkhan75830@gmail.com>
2024-12-02 11:50:46 +0500

    Tween Animation

---
+++ lib/example6.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Example6 extends StatefulWidget {
+  const Example6({super.key});
+
+  @override
+  State<Example6> createState() => _Example6State();
+}
+
+class _Example6State extends State<Example6> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
