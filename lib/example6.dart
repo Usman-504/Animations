ca48b3a6e1aa7509b88e32e80de26e9eaaf28ba8
Muhammad Usman <ahmadkhan75830@gmail.com>
2024-12-02 12:04:15 +0500

    Tween Animation

--- lib/example6.dart
+++ lib/example6.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class Example6 extends StatefulWidget {
   const Example6({super.key});
@@ -6,10 +7,40 @@ class Example6 extends StatefulWidget {
   @override
   State<Example6> createState() => _Example6State();
 }
-
+Color getRandomColor() =>
+    Color(0xFF000000 + math.Random().nextInt(0x00FFFFFF));
 class _Example6State extends State<Example6> {
+
+  var _color = getRandomColor();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: TweenAnimationBuilder(
+          tween: ColorTween(
+            begin: getRandomColor(),
+            end: _color,
+          ),
+          onEnd: (){
+            setState(() {
+              _color = getRandomColor();
+            });
+      },
+          duration: const Duration(seconds: 1),
+          builder: (context, Color? color, child) {
+            return ColorFiltered(
+              colorFilter: ColorFilter.mode(color!, BlendMode.srcATop),
+              child: Container(
+                height: MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Colors.red),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
